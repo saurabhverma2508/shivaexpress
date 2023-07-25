6b930af658e64081a8709211fed25c53d4d2ecf7
saurabhverma2508 <118890348+saurabhverma2508@users.noreply.github.com>
2023-07-25 18:24:37 +0530

    Update Tracking.js

--- db/Tracking.js
+++ db/Tracking.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose")
 
+const addTrackingSchema= new mongoose.Schema({
+    trackingid:{type:String,unique:true},
+    description:String
+},
+{
+    collection:"trackings"
+}
+)
+
+
+module.exports=mongoose.model("trackings",addTrackingSchema)
