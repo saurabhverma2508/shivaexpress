4fc7d8ce355e0a6bcf734bc3f6bd400e83a1a880
saurabhverma2508 <118890348+saurabhverma2508@users.noreply.github.com>
2023-07-25 18:23:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-
-
 const express = require("express");
 const cors = require("cors");
 require("./db/config");
 const Login = require("./db/Login");
-
+const Tracking = require("./db/Tracking");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req,res)=>{
-res.send("Api is there")
-})
-
-
-
+// ****Login Api**** //
 app.post("/login", async (req, res) => {
   if (req.body.email && req.body.password) {
     let user = await Login.findOne(req.body);
     if (user) {
       res.send({
-        userEmail :user.email,
+        userEmail: user.email,
         message: "Api run successfully",
         status_code: 200,
       });
@@ -35,6 +28,130 @@ app.post("/login", async (req, res) => {
     res.send({ message: "Something went wrong", status_code: 400 });
   }
 });
+
+//******* Add Tracking Api ******//
+app.post("/addtracking", async (req, res) => {
+  const { trackingid, description } = req.body;
+  try {
+    const already = await Tracking.findOne({ trackingid });
+    if (already) {
+      res.send({
+        message: "Tracking id is already exist",
+        status_code: 300,
+      });
+    } else {
+      await Tracking.create({
+        trackingid,
+        description,
+      });
+      res.send({
+        result: req.body,
+
+        message: "Api run successfully",
+        status_code: 200,
+      });
+    }
+  } catch (error) {
+    res.send("error");
+  }
+});
+
+//******* All Tracking Api ******//
+app.get("/alltracking", async (req, res) => {
+  try {
+    const alldata = await Tracking.find({});
+    res.send({
+      results: alldata,
+
+      message: "Api run successfully",
+      status_code: 200,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//******* View Tracking Api ******//
+app.post("/viewtracking", async (req, res) => {
+  try {
+    let viewData = await Tracking.findOne(req.body);
+    if (viewData) {
+      res.send({
+        result: [viewData],
+        message: "Api run successfully",
+        status_code: 200,
+      });
+    } else {
+      res.send({
+        result: (viewData = []),
+        message: "Data Not Found",
+        status_code: 300,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//******* Delete Tracking Api ******//
+app.post("/deletetracking", async (req, res) => {
+  const { trackingid } = req.body;
+
+  try {
+    let getdata = await Tracking.findOne({trackingid})
+    if(getdata){
+      let deleteddata = await Tracking.deleteOne({ trackingid: trackingid });
+      if (deleteddata.body !== "undefined") {
+        res.send({
+          message: "Data deleted successfully",
+          status_code: 200,
+        });
+        
+      } else {
+        res.send({
+          message: "Data Not Found",
+          status_code: 300,
+        });
+      }
+    }else{
+      res.send({
+        message: "Data Not Found",
+        status_code: 300,
+      });
+    }
+    
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//******* Update Tracking Api ******//
+// app.put("/updatetracking",async (req,res)=>{
+// const {trackingid}=req.body
+//   try{
+//     let updatedata= await Tracking.findOneAndUpdate({trackingid:req.params.id},{
+//       $set:{
+//         description:req.body.description
+//       }
+//     })
+//     if(updatedata){
+//       res.send({
+    
+//           message: "Data Updated Successfully",
+//           status_code: 200,
+  
+//       })
+//     }
+    
+ 
+//   }
+// catch(error){
+// console.log(error)
+// }
+// })
+
+
+
 
 app.listen(5000, () => {
   console.log("server started at 5000");
